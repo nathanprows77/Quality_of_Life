81cae071e4c2f6173e9ba22c78df3d253c50e8ae
Tomorrow Rose <tomorrowrose75@gmail.com>
2020-11-12 20:04:55 -0500

    add app.py

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
-from flask import Flask, request
-
+# app.py to run this code run: python app.py
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def respond():
+    return app.send_static_file('index.html')
+    # # Retrieve the name from url parameter
+    # name = request.args.get("name", None)
 
-@app.route("/name/<name>")
-def get_book_name(name):
-    return "name : {}".format(name)
+    # # For debugging
+    # print(f"got name {name}")
 
-@app.route("/details")
-def get_book_details():
-   author=request.args.get('author')
-   published=request.args.get('published')
-  return "Author : {}, Published: {}".format(author,published)
+    # response = {}
+
+    # # Check if user sent a name at all
+    # if not name:
+    #     response["ERROR"] = "no name found, please send a name."
+    # # Check if the user entered a number not a name
+    # elif str(name).isdigit():
+    #     response["ERROR"] = "name can't be numeric."
+    # # Now the user entered a valid name
+    # else:
+    #     response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # # Return the response in json format
+    # return jsonify(response)
 
 if __name__ == '__main__':
     app.run()
